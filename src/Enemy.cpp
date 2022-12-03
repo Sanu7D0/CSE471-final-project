779b048d3afb9220ca6ba376ae7bf6d78f2f1e82
100gging <117820966+100gging@users.noreply.github.com>
2022-12-03 15:38:26 +0900

    Update Enemy.cpp

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -4,7 +4,8 @@ void Enemy::update(const float dt)
 {
 	glm::vec3 chaseDir = targetPosition - transform.position;
 	chaseDir = glm::normalize(chaseDir);
-	//transform.rotation = 
+	theta = glm::acos(glm::dot(chaseDir, (0.0f, 0.0f, 1.0f))/(glm::length(chaseDir)*glm::length(0.0f,0.0f,1.0f)))
+	transform.rotation = glm::quat(glm::vec3(0.0f,glm::radians(theta), 0,0f)*glm::vec3(0.0f, 0.0f, 1.0f))
 
 	RigidBody::update(dt);
 }
